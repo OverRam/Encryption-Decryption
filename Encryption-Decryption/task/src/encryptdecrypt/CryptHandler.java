3ad5def0c1924d86315e0e2511ce879fadd1d73d
Dominik <77927514+OverRam@users.noreply.github.com>
2021-08-25 22:23:53 +0200

    ChoseCrypt method changed, matching the pattern of arguments passed from the command line

--- Encryption-Decryption/task/src/encryptdecrypt/CryptHandler.java
+++ Encryption-Decryption/task/src/encryptdecrypt/CryptHandler.java
@@ -1,19 +1,31 @@
 package encryptdecrypt;
 
-import java.util.Scanner;
-
 public class CryptHandler {
 
-    static void choseCrypt(String chose, String message) {
-        Scanner sc = new Scanner(System.in);
-        switch (chose) {
+    static void choseCrypt(String[] arguments) {
+        String mode = "enc";
+        String data = "";
+        int key = 0;
+
+        for (int i = 0; i < arguments.length; i += 2) {
+            if ("-mode".equals(arguments[i])) {
+                mode = arguments[i + 1];
+            } else if ("-key".equals(arguments[i])) {
+                key = Integer.parseInt(arguments[i + 1]);
+            } else if ("-data".equals(arguments[i])) {
+                data = arguments[i + 1];
+            }
+        }
+
+        switch (mode) {
             case "enc":
-                Encrypted ec = new Encrypted(message);
-                System.out.println(ec.shiftLetterByValue(sc.nextInt()));
+                Encrypted ec = new Encrypted(data);
+                System.out.println(ec.shiftLetterByValue(key));
                 break;
             case "dec":
-                Decrypted dec = new Decrypted(message);
-                System.out.println(dec.shiftLetterByValue(sc.nextInt()));
+                Decrypted dec = new Decrypted(data);
+                System.out.println(dec.shiftLetterByValue(key));
         }
     }
+
 }
