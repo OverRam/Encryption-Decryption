031d14216e270158edccc401c6d66aea59ddb138
Dominik <77927514+OverRam@users.noreply.github.com>
2021-08-26 23:54:01 +0200

    change of startup method

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -2,6 +2,8 @@ package encryptdecrypt;
 
 public class Main {
     public static void main(String[] args) {
+//        String[] test = {"-mode", "dec", "-in", "road_to_treasure.txt", "-out", "saveFile.txt", "-key", "5"};
+
         argumentsHandler.handler(args);
     }
 }
