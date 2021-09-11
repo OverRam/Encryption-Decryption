89b12f6dd6efdd598ee4acad8109abb1c1d08b53
Dominik <77927514+OverRam@users.noreply.github.com>
2021-09-11 18:21:22 +0200

    End

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -2,8 +2,6 @@ package encryptdecrypt;
 
 public class Main {
     public static void main(String[] args) {
-//        String[] test = {"-mode", "dec", "-in", "road_to_treasure.txt", "-out", "saveFile.txt", "-key", "5"};
-
         argumentsHandler.handler(args);
     }
 }
